e756360fc9ace07bceeb634bca1e5d94607eb1f0
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-31 20:44:56 +0200

    probando estructura de proyecto

--- store/main.py
+++ store/main.py
@@ -2,3 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods= ['*'],
+    allow_headers= ['*']
+)
+
+redis = get_redis_connection(
+    host='redis-12878.c300.eu-central-1-1.ec2.cloud.redislabs.com',
+    port=12878,
+    password='0TOCYuEuly0piSZWRZn8aTIoOSLKVlWH',
+    decode_responses= Truegit
+)
